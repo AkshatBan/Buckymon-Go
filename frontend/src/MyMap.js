af44583dea98fd2df4dad74baf436198d913da72
Agastya-Asthana <aasthana3@wisc.edu>
2024-03-03 18:51:18 -0600

    Added working Mabox Implementation that just simply loads in a new map for the full screen

---
+++ frontend/src/MyMap.js
@@ -0,0 +1,18 @@
+import ReactMapboxGl from "react-mapbox-gl";
+
+
+function MyMap(){
+    const Map = ReactMapboxGl({
+        accessToken: "pk.eyJ1IjoiYWFzdGhhbmEzIiwiYSI6ImNsdGM1aXU1ZjF4OWUyam8weGczeXZsNjYifQ.qglYcmv9q6XjL1NvXk9szw"
+    })
+    return(
+        <Map style="mapbox://styles/mapbox/streets-v8"
+        containerStyle={{
+            height: '100vh',
+            width: '100vw'
+        }}
+        center={[-89.401230, 43.073051]} />
+    );
+}
+
+export default MyMap;
