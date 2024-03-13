343dce968e88e135139abf946732353496bb2b9a
Grant Hall <gghall@wisc.edu>
2024-03-12 22:32:31 -0500

    Implemented simple connection between frontend and backend

--- frontend_2/src/MyMarker.js
+++ frontend_2/src/MyMarker.js
@@ -4,6 +4,31 @@ import { Marker, Popup } from "react-map-gl";
 function MyMarker(props){
 
     const [selected, Set_Selected] = React.useState(false);
+    const [response_data, Set_Response_Data] = React.useState(null);
+
+    //asynchronous function because post request takes time
+    const Handle_Complete = async () => {
+        try {
+            const response = await fetch("http://127.0.0.1:5000/api/Get_Data_From_Frontend", {
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json'
+                },
+                //dummy data for now, more to be added later
+                body: JSON.stringify({ 
+                    lat: props.latitude,
+                    long: props.longitude,
+                    id: props.id
+                })
+            });
+            const data = await response.json();
+            Set_Response_Data(data);
+            } catch (error) {
+            console.error('Error:', error);
+            }
+    };
+    
+
 
 
     return(
@@ -12,9 +37,10 @@ function MyMarker(props){
                 key={props.id}
                 latitude={props.latitude}
                 longitude={props.longitude}
-                onClick={() => Set_Selected(true)}
+                onClick={() => {Set_Selected(true)}}
             />
 
+            {/* make popup appear when the marker is clickes */}
             {selected ? (
                 <Popup
                     latitude={props.latitude}
@@ -23,15 +49,18 @@ function MyMarker(props){
                     onClose={() => Set_Selected(false)}
                 >
                         <p>{props.message}</p>
+                        {/* send post request to backend when completion is marked */}
                         <button 
-                            style={{"background-color": "lime"}} 
+                            style={{"backgroundColor": "lime"}} 
                             onClick={() => {
                                 Set_Selected(false);
+                                Handle_Complete()
                             }}>
                             Complete Event
                         </button>
                         <br></br>
-                        <button style={{"background-color": "red"}} onClick={() => Set_Selected(false)}>
+                        {/* close popup when close */}
+                        <button style={{"backgroundColor": "red"}} onClick={() => Set_Selected(false)}>
                             Close
                         </button>
                 </Popup>
