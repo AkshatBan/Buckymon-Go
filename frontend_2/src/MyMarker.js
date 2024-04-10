97f57a36f8c4164533037aca093dcd66e1f67f27
Grant Hall <gghall@wisc.edu>
2024-04-09 20:37:34 -0500

    Updated frontend code to make changes necessary for integration meeting

--- frontend_2/src/MyMarker.js
+++ frontend_2/src/MyMarker.js
@@ -9,7 +9,7 @@ function MyMarker(props){
     // asynchronous function because post request takes time
     const Handle_Complete = async () => {
         try {
-            const response = await fetch("http://127.0.0.1:5000/api/Get_Data_From_Frontend", {
+            const response = await fetch("http://127.0.0.1:5000/api/Complete_Event", {
                 method: 'POST',
                 headers: {
                 'Content-Type': 'application/json'
@@ -18,10 +18,12 @@ function MyMarker(props){
                 body: JSON.stringify({ 
                     lat: props.latitude,
                     long: props.longitude,
-                    id: props.id
+                    id: props.id,
+                    user: "grant_hall"
                 })
             });
             const data = await response.json();
+            alert(`New Score: ${data.updated_score}`)
             Set_Response_Data(data);
             } catch (error) {
             console.error('Error:', error);
@@ -53,7 +55,7 @@ function MyMarker(props){
                         <button 
                             style={{"backgroundColor": "lime"}} 
                             onClick={() => {
-                                Set_Selected(false)
+                                Handle_Complete()
                             }}>
                             Complete Event
                         </button>
