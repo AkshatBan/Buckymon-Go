b9fd8eac569f2eeb5d8985676401968b54395ad8
Jasper Bo <jjbo@wisc.edu>
2024-04-23 23:46:15 -0500

    Deleted outdated post_data_to_frontend test

--- Backend/TestFlaskAPI.py
+++ Backend/TestFlaskAPI.py
@@ -67,25 +67,6 @@ class TestFlaskAPI(unittest.TestCase):
             }
         ]
         self.assertEqual(response, json.dumps(expected))
-
-    # @patch('app.location_data') # replaces the location data with a mock obj
-    # def test_post_data_to_frontend(self, mock_location_data):
-    #     # this will be the mock data that we replace the location data with
-    #     mock_location_data.return_value = [
-    #         {'id': 1, 'long': -89.408, 'lat': 43.0719, 'location_name': 'Union South', 'event_desc': 'description'},
-    #         {'id': 2, 'long': -89.404, 'lat': 43.0757, 'location_name': 'Bascom Hall', 'event_desc': 'something'}
-    #     ]
-        
-    #     # retrieve (not mocked) response from test client
-    #     response = self.client.get('/api/Post_Data_To_Frontend')
-        
-    #     # asser that the retrieved response is successful
-    #     self.assertEqual(response.status_code, 200)
-        
-    #     # make sure that we're actually getting json data and that
-    #     # there is only 2 items (since our mock only has 2)
-    #     self.assertTrue(isinstance(response.json, list))
-    #     self.assertEqual(len(response.json), 2)
         
     # @patch('Frontend_To_Backend_Data_Passage.request') # replaces the request import with a mock import
     @patch('Frontend_To_Backend_Data_Passage.pymysql.connect') # replaces pymysql connect import with a mock    
